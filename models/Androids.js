af8ce292f18902ecd793db869be89023f0dc6d0c
Akash-Raj <asrivastav722@gmail.com>
2026-09-13 14:28:59 +0530

    commit

--- models/Androids.js
+++ models/Androids.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const androidSchema = new mongoose.Schema({
   token: { type: String, required: true, unique: true ,sparse: true},
+  deviceHardwareId: { 
+    type: String, 
+    unique: true, 
+    sparse: true // <--- Crucial: tells MongoDB to ignore null/missing values
+  },
   createdAt: { type: Date, default: Date.now },
 });
 
